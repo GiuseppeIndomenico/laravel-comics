a8e2161672678aa201a4f209575f648663600ffa
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-05-22 20:08:19 +0200

    fix comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,8 +3,7 @@
 @section('content')
     <div class="row">
         @foreach ($comics as $comic)
-            <div v-for="(comic, index) in comics" :key="index" class="col-12 col-sm-6 col-md-4 col-lg-2">
-
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
                 <div class="d-flex flex-column align-content-center justify-content-center py-2">
                     <div class="gi-img mb-2">
                         <img src="{{ $comic['thumb'] }}" alt="type">
